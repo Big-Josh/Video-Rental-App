ae2057037f8c68c8c0a1e2e5a32ca6373c2c365f
Big Josh <otoojoshua616@gmail.com>
2024-09-29 19:07:36 +0000

    Http request

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Hello Word')
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/'){
+        res.write('Hello Josh');
+        res.end;
+    }
+});
+
+server.on('connection', (socket) => {
+    console.log('New connection ....')
+})
+server.listen(3000);
+
+console.log('Listening on 30000')
