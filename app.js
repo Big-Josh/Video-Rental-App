c9dc9917abf7786dfe184fa451952eb77c647d11
Big Josh <otoojoshua616@gmail.com>
2024-10-06 04:19:35 +0000

    Input Validation and PUT request

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const Joi = require('joi')
 const express = require('express')
 const app = express();
+
 
 app.use(express.json());
 
@@ -25,6 +27,13 @@ app.get('/api/courses/:id', (req,res) => {
 });
 
 app.post('/api/courses/', (req,res) => {
+    
+    const {error} = validateCourse(req.body)
+    
+   if (error){
+    res.status(400).send(error.details[0].message);
+    return;
+   }
     const course = {
         id: courses.length + 1,
         name:  req.body.name
@@ -32,6 +41,29 @@ app.post('/api/courses/', (req,res) => {
     courses.push(course)
     res.send(course)
 });
+
+
+app.put('/api/courses/:id', (req,res) => {
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send("Course with given id not found")
+    
+    const {error} = validateCourse(req.body)
+    if (error){
+        res.status(400).send(error.details[0].message)
+       }
+
+    course.name = req.body.name
+    res.send(course)
+    
+})
+
+function validateCourse(course){
+    const schema = Joi.object({
+        name : Joi.string().min(3).required()
+    });
+    return schema.validate(course.body);
+
+}
 //PORT 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log('Listening on port 3000'))
