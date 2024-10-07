3d326226182f92dd011b68b145eade3bd2a039e6
Big Josh <otoojoshua616@gmail.com>
2024-10-07 23:39:27 +0000

    MiddleWare@

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const Joi = require('joi')
 const express = require('express')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const logger = require('./logger');
+const authenticator = require('./authenticator');
 const app = express();
 
 
 app.use(express.json());
+app.use(helmet())
+app.use(morgan())
+app.use(express.urlencoded());
+app.use(express.static('public'));
+//app.use(logger);
+//app.use(authenticator);
+
 
 const courses = [
     {id :1, name : 'course1'},
@@ -22,7 +33,7 @@ app.get('/api/courses', (req,res) => {
 
 app.get('/api/courses/:id', (req,res) => {
     let course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send("Course with given id not found")
+    if (!course) return res.status(404).send("Course with given id not found")
     res.send(course)    
 });
 
@@ -30,10 +41,9 @@ app.post('/api/courses/', (req,res) => {
     
     const {error} = validateCourse(req.body)
     
-   if (error){
-    res.status(400).send(error.details[0].message);
-    return;
-   }
+   if (error) return res.status(400).send(error.details[0].message);
+    
+   
     const course = {
         id: courses.length + 1,
         name:  req.body.name
@@ -42,20 +52,32 @@ app.post('/api/courses/', (req,res) => {
     res.send(course)
 });
 
-
+// UPDATE
 app.put('/api/courses/:id', (req,res) => {
     let course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send("Course with given id not found")
+    if (!course) return res.status(404).send("Course with given id not found")
     
     const {error} = validateCourse(req.body)
-    if (error){
-        res.status(400).send(error.details[0].message)
-       }
+    if (error) return res.status(400).send(error.details[0].message)
+       
 
     course.name = req.body.name
     res.send(course)
     
 })
+
+//Delete
+
+app.delete('/api/courses/:id', (req,res) => {
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send("Course with given id not found")
+
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+
+    res.send(course);
+    
+})   
 
 function validateCourse(course){
     const schema = Joi.object({
